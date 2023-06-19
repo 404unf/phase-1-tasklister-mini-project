7efaf390e920cabcdd99169e2c11ae57190e0bb9
Anna <34191811+404unf@users.noreply.github.com>
2023-06-18 23:37:17 -0700

    Add and delete tasks

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  const input = document.getElementById('create-task-form');
+  const currentList = document.getElementById('tasks');
+  const deleteButton = document.createElement('button');
+  deleteButton.className = 'delete'
+  deleteButton.textContent = 'X'
+
+  input.addEventListener('submit', function (event) {
+    event.preventDefault()
+
+    const text = document.getElementById('new-task-description').value
+    const newLi = document.createElement('li')
+    const newButton = deleteButton.cloneNode(true)
+    newButton.addEventListener('click', (event) => { event.target.parentElement.remove() })
+
+    newLi.textContent = text
+    newLi.appendChild(newButton)
+
+    currentList.appendChild(newLi)
+  }); 
+})
